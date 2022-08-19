90c65bc9ef1f1ac8de9bb2c307121cfc7dffb259
Rki0 <pky00823@gmail.com>
2022-08-19 11:46:29 +0900

    other route complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,6 @@ const { User } = require("../model/User");
 
 const auth = (req, res, next) => {
   if (req.session.logined) {
-    // console.log("yes you are logined", req.session);
-
     // session 정보에 들어가 있는 유저를 인증 유저로 설정?
     req.user = req.session;
 
